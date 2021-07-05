61c756b7f47cfad6d94ab4adcddd00e030411d1d
Josue Arreola <47585614+josh231101@users.noreply.github.com>
2021-07-04 20:41:56 -0500

    Debuggin and componentDidCatch Esta clase que acabamos de hacer es genérica para wrappear toda al app y controlar los errores 😵

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./styles.css";
 import Section12 from "./Sections/12";
 import Section6 from "./Sections/6";
+import Section8 from "./Sections/8";
 export default function App() {
   return (
     <div className="App">
       <Section6 />
+      <Section8 />
       <Section12 />
     </div>
   );
--- src/Sections/8/index.js
+++ src/Sections/8/index.js
@@ -3,14 +3,52 @@ import React, { Component, useRef } from "react";
 /**
  * Sección 8: Debuggin y control de Errores
  */
+class LimiteErrores extends Component {
+  state = {
+    tieneError: false
+  };
+  componentDidCatch(error, errorInfo) {
+    this.setState({ tieneError: true, error });
+  }
 
-const Section6 = () => {
+  render() {
+    const { tieneError, error, errorInfo } = this.state;
+    if (tieneError) {
+      return (
+        <div>
+          UPs! Ocurrió un error inesperado en la aplicación :/
+          <h2>{!!error && error.toString()}</h2>
+        </div>
+      );
+    }
+    return this.props.children;
+  }
+}
+
+class FailedButton extends Component {
+  state = {
+    dispararError: false
+  };
+  dispararError = () => this.setState({ dispararError: true });
+
+  render() {
+    if (this.state.dispararError) {
+      throw new Error("Lo siento he fallado ️‍🔥️‍🔥️‍🔥");
+    }
+    return <button onClick={this.dispararError}>Button with Bug</button>;
+  }
+}
+
+const Section8 = () => {
   return (
     <>
       <h1>Sección 8: Debuggin y control de Errores</h1>
       <span>El poderoso Chrome DevTools</span>
+      <LimiteErrores>
+        <FailedButton />
+      </LimiteErrores>
     </>
   );
 };
 
-export default Section6;
+export default Section8;
