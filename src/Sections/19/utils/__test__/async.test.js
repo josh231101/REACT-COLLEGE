ab4c65a8a7d778b23b6e4659b2df9a91c79ab8ad
Josue Arreola <47585614+josh231101@users.noreply.github.com>
2021-07-18 20:09:02 -0500

    Testing asyn await from REST API

--- src/Sections/19/utils/__test__/async.test.js
+++ src/Sections/19/utils/__test__/async.test.js
@@ -1,4 +1,9 @@
-import { getDataCallback, getDataPromise, getDataPromiseError } from "../async";
+import {
+  getDataCallback,
+  getDataPromise,
+  getDataPromiseError,
+  getUsers
+} from "../async";
 
 describe("HAciendo test a operaciones asíncronas", () => {
   test("Haciendo test a callback", (done) => {
@@ -18,5 +23,24 @@ describe("HAciendo test a operaciones asíncronas", () => {
   });
   test("Haciendo test a promesa rechazada", () => {
     return expect(getDataPromiseError()).rejects.toBe("Error");
+  });
+  test("Haciendo test a promesa resuelta con async await", async () => {
+    const name = await getDataPromise();
+    expect(name).toBe("Josue Arreola");
+  });
+  test("Haciendo test a promesa resuelta fallida con async/await", async () => {
+    try {
+      const name = await getDataPromiseError();
+      expect(name).toBe("Josue Arreola");
+    } catch (e) {
+      expect(e).toBe("Error");
+    }
+  });
+
+  test("Probando promesa con solicitud HTTP", async () => {
+    const user = await getUsers();
+    expect(user).toHaveProperty("username");
+    expect(user).toHaveProperty("id");
+    expect(user).toHaveProperty("email");
   });
 });
--- src/Sections/19/utils/async.js
+++ src/Sections/19/utils/async.js
@@ -4,14 +4,14 @@ export const getDataCallback = (callback) => {
 
   setTimeout(() => {
     callback(name);
-  }, 2000);
+  }, 100);
 };
 
 export const getDataPromise = () => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve("Josue Arreola");
-    }, 2000);
+    }, 100);
   });
 };
 
@@ -19,6 +19,12 @@ export const getDataPromiseError = () => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       reject("Error");
-    }, 2000);
+    }, 100);
   });
+};
+
+export const getUsers = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users/5");
+  const user = await res.json();
+  return user;
 };
